140695d9ade2b2dece8ba97b60ab7fc08d826ae6
lms0827 <54874505+lms0827@users.noreply.github.com>
2020-04-10 14:51:13 +0900

    init

---
+++ src/test/java/com/minseok/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minseok.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
